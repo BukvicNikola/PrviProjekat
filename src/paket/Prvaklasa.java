3a0511bd6e397ba5acc2cf354961bb761a63cedf
BukvicNikola <nikibuki2006@gmail.edu.rs>
2023-09-14 17:36:30 +0200

    Initial comit

---
+++ src/paket/Prvaklasa.java
@@ -0,0 +1,13 @@
+package paket;
+
+public class Prvaklasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			//prvi komentar
+		/*viselinijski
+		 * komentar
+		 */
+	}
+
+}
